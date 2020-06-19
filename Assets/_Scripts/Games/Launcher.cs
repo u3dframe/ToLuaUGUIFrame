bf9ad81f912daba03d03d3dc53563971af680ff5
canyon <gongyanghui1986@163.com>
2020-06-19 11:10:58 +0800

    代码整理

---
+++ Assets/_Scripts/Games/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gobj = new GameObject("LuaLooper",typeof(GameLuaClient));
+        GameObject.DontDestroyOnLoad(gobj);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {    
+    // }
+}
---
+++ Assets/_Scripts/Games/ToLua/GameLuaClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+
+public class GameLuaClient : LuaClient
+{
+    protected override LuaFileUtils InitLoader()
+    {
+        return new LuaFileLoader();
+    }
+
+    protected override void OpenLibs()
+    {
+        base.OpenLibs();
+		InitSelfLibs();
+    }
+	
+	void InitSelfLibs()
+	{
+		/*
+        luaState.BeginPreLoad();
+        luaState.RegFunction("sproto.core", luaopen_sproto_core);
+        luaState.EndPreLoad();
+		*/
+	}
+	
+	protected override void LoadLuaFiles()
+	{
+		InitLuaPath();
+		base.LoadLuaFiles();
+	}
+
+	public void LuaGC(){
+		luaState.LuaGC(LuaGCOptions.LUA_GCCOLLECT);
+	}
+	
+	/// <summary>
+	/// 初始化Lua代码加载路径
+	/// </summary>
+	void InitLuaPath() {
+		string rootPath = Application.dataPath;
+#if UNITY_EDITOR
+		luaState.AddSearchPath(rootPath + "/ToLua/Lua");
+#else
+		rootPath = Application.dataPath;
+#endif
+		luaState.AddSearchPath(rootPath + "/Lua");
+	}
+}
